f0b5122707cdf7b2fa05f3860e80603f46eba2db
hkhan105 <93456716+hkhan105@users.noreply.github.com>
2021-12-06 19:20:38 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,10 @@ var seaImg,shipImg;
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1");
-  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+ 
+ 
+  shipImg1 = loadAnimation("ship-2.png","ship-1.png","ship-2.png","ship-1.png");
+  
   
   seaImg = loadImage("sea.png");
 }
@@ -20,7 +19,7 @@ function setup(){
   // Moving background
   sea=createSprite(400,200);
   sea.addImage(seaImg);
-  sea.velocityX = -5;
+  sea.velocityX = 3;
   sea.scale=0.3;
 
   
@@ -32,14 +31,12 @@ function setup(){
 
 function draw() {
   background(0);
-  sea.velocityX = -3;
+  sea.velocityX = 3;
 
   //uncomment code to reset the background
-  if(sea.x < 0){
+  if(sea.x > 400){
     sea.x = 0;
-    sea.x = sea.width;
-    sea.x = sea.width/8;
-    sea.y = height;
+    sea.y = 200;
   }
 
  
